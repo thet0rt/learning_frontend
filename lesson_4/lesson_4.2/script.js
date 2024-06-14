84123bd1450bf44450b65e3148863718fcc915f6
Георгий Пузиков <bythetort@gmail.com>
2024-06-14 20:21:10 +0300

    learned how to customize actions on the elements

--- lesson_4/lesson_4.2/script.js
+++ lesson_4/lesson_4.2/script.js
@@ -19,10 +19,65 @@ $(function () {
 $(function () {
 
     function changeFontColor() {
-        $('#container').addClass('newStyle');
+        $('#container').toggleClass('newStyle');
     }
-    setTimeout(changeFontColor, 1000)
+
+    // setTimeout(changeFontColor, 1000)
+
+    $("#changeColor").on("click", function () {
+        changeFontColor();
+    });
 });
+
 
 // removeClass - удаляет класс
 // toggleClass - удаляет класс, если есть, добавляет если нет
+
+// Какие еще бывают события?
+//     - on click - по нажатию
+//     - on mouseover - по наведению мышки
+//     - on mouseout - когда мы мышь уводим
+//     - dblclick - по двойному клику
+
+
+// Есть еще события клавиатуры
+
+
+$(function () {
+
+    function changeFontColor() {
+        $('.container').toggleClass('newStyle');
+    }
+
+    // setTimeout(changeFontColor, 1000)
+
+    // $("textarea").on("keydown", function (event) {
+    //     changeFontColor();
+    //     console.log(event.which);
+    //     if (event.which < 48 || event.which > 57)
+    //     {
+    //         return false;
+    //     }
+    // }).on('focus', function () {
+    //     changeFontColor();
+    // }).on('blur', function () {
+    //     changeFontColor();
+    // });
+    $("textarea").on("focus", function(){
+        $(this).css('background-color', 'pink');
+    });
+    $("textarea").on("blur", function(){
+        $(this).css('background-color', 'white');
+    });
+
+    $("a").on('click', function (){
+        changeFontColor();
+        return false;
+    })
+
+});
+
+// keydown - кнопка вниз
+// keyup - кнопка вверх
+// keypress
+
