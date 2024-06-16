d634626d646e85af9c61ecbcca430fc9be59e880
Георгий Пузиков <bythetort@gmail.com>
2024-06-16 16:20:04 +0300

    learned how to add/remove/edit elements in jquery

--- lesson_4/lesson_4.4/script.js
+++ lesson_4/lesson_4.4/script.js
@@ -8,3 +8,48 @@ $(function() {
         $('.news-time').css('color', 'red')
     });
 })
+
+$(function () {
+    $('#changeCommentsButton').css(
+        {
+            'margin': '20px'
+        }
+    ).click(function () {
+        $('.news-comments-count').each(function (){
+            if ($(this).text() > 20) {
+                $(this).css('color', 'blue');
+                console.log($(this).text());
+            }
+        })
+    })
+})
+
+
+// С помощью метода parent можно обращаться к родительскому элементу
+// Метод parents просматривает всех родителей
+// .next() выберет не этот элемент, а следующий. previous() - предыдущий
+// .children() - ко всем элементам внутри родительского элемента
+
+$(function (){
+    $(".news-time").click(function () {
+        $(this).parent().parent().css('border', '1px solid red')
+    // Можно менять не только стили, но и содержимое элементов
+    // }).click(function (){
+    //     $(".news-item").first().html('<small>Мелкий текст</small>')
+        //Можно удалить элемент
+    }).click(function () {
+        $('#column-left h2').remove();
+        //Можно добавить элемент
+    }).click(function () {
+        $('#column-left').prepend('<h2>Новый заголовок новостей</h2>');
+        // Добавить в конец
+    }).click(function () {
+        $('#column-left').append('<h2>Новый заголовок новостей</h2>');
+        // Можно записать новый элемент в переменную
+    }).click(function () {
+        var newElement = $('<h2></h2>');
+        newElement.text('Срочные новости');
+        $('#column-left').prepend(newElement)
+    })
+})
+
